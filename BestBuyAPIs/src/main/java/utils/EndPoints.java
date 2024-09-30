6d83ffc931e7f792e66a92e9f6bca7c1979d22d9
NouranElsaraf <111913684+NouranElsaraf@users.noreply.github.com>
2024-09-30 05:31:36 +0300

    Delete BestBuyAPIs directory

--- BestBuyAPIs/src/main/java/utils/EndPoints.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class EndPoints {
-
-        public static final String GET_PRODUCTS = "/products";
-        public static final String GET_PRODUCT_BY_ID = "/products/{id}";
-        public static final String CREATE_PRODUCT = "/products";
-        public static final String DELETE_PRODUCT = "/products/{id}";
-}
